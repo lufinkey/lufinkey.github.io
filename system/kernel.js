08205ad8f7f48b7f258e883b8b842d53c13391af
lufinkey <luisfinke@gmail.com>
2018-02-26 16:35:45 -0500

    make kernel filesystem synchronous

--- system/kernel.js
+++ system/kernel.js
@@ -1,7 +1,6 @@
 
 // sandbox kernel data
 (function(){
-
 	const osName = 'finkeos';
 
 	// Filesystem class
@@ -54,27 +53,23 @@
 		// get metadata about item at path
 		function readMeta(path)
 		{
-			return new Promise((resolve, reject) => {
-				if(!validatePath(path))
+			if(!validatePath(path))
+			{
+				throw new Error("invalid path");
+			}
+			var meta = storage.getItem(fsMetaPrefix+path);
+			if(meta != null)
+			{
+				try
 				{
-					reject(new Error("invalid path"));
-					return;
+					meta = JSON.parse(meta);
 				}
-				var meta = storage.getItem(fsMetaPrefix+path);
-				if(meta != null)
+				catch(error)
 				{
-					try
-					{
-						meta = JSON.parse(meta);
-					}
-					catch(error)
-					{
-						reject("corrupted entry meta");
-						return;
-					}
+					throw new Error("corrupted entry meta");
 				}
-				resolve(meta);
-			});
+			}
+			return meta;
 		}
 
 		// create a default meta object
@@ -101,128 +96,95 @@
 		// write an entry
 		function writeEntry(path, meta, data)
 		{
-			return new Promise((resolve, reject) => {
-				// validate data
-				if(typeof data !== 'string')
+			// validate data
+			if(typeof data !== 'string')
+			{
+				throw new Error("non-string data may not be written");
+			}
+
+			// get info about potentially already existing entry
+			var entryMeta = readMeta(path);
+
+			// validate containing directory
+			var dirPath = dirname(path);
+			var dirMeta = readMeta(dirPath);
+			if(dirMeta == null)
+			{
+				throw new Error("containing directory does not exist");
+			}
+			else if(dirMeta.type !== 'dir')
+			{
+				throw new Error("invalid containing directory");
+			}
+
+			var dirData = readDir(dirPath);
+
+			// create new meta data
+			var meta = entryMeta;
+			if(meta == null)
+			{
+				// create default meta
+				meta = createMeta(meta);
+			}
+			else
+			{
+				// validate existing meta
+				if(meta.type !== entryMeta.type)
 				{
-					reject(new Error("non-string data may not be written"));
-					return;
+					throw new Error("overwriting entry type mismatch");
 				}
+			}
 
-				let entryMeta = null;
-				let dirPath = null;
-				let dirMeta = null;
+			// add updated entry meta info
+			meta.dateUpdated = new Date().getTime();
 
-				// get info about potentially already existing entry
-				readMeta(path).then((meta) => {
-					entryMeta = meta;
-					// read meta info of containing directory
-					dirPath = dirname(path);
-					return readMeta(dirPath);
-				}).catch((error) => {
-					reject(error);
-				}).then((meta) => {
-					// validate containing directory
-					dirMeta = meta;
-					if(dirMeta == null)
-					{
-						reject(new Error("containing directory does not exist"));
-						return;
-					}
-					else if(dirMeta.type !== 'dir')
-					{
-						reject(new Error("invalid containing directory"));
-						return;
-					}
+			// add updated parent dir entry meta info
+			dirMeta.dateUpdated = new Date().getTime();
 
-					// read contents of containing directory
-					return readDir(dirPath);
-				}).catch((error) => {
-					reject("unable to get meta info on containing directory: "+error.message);
-				}).then((dirData) => {
-					// create new meta data
-					var newMeta = entryMeta;
-					if(newMeta == null)
-					{
-						// create default meta
-						newMeta = createMeta(meta);
-					}
-					else
-					{
-						// validate existing meta
-						if(newMeta.type !== meta.type)
-						{
-							reject(new Error("overwriting entry type mismatch"));
-							return;
-						}
-					}
+			// add entry to parent directory contents, if not already added
+			var entryName = basename(path);
+			if(!dirData.includes(entryName))
+			{
+				dirData.push(entryName);
+			}
 
-					// add updated entry meta info
-					newMeta.dateUpdated = new Date().getTime();
-
-					// add updated parent dir entry meta info
-					dirMeta.dateUpdated = new Date().getTime();
-
-					// add entry to parent directory contents, if not already added
-					var entryName = basename(path);
-					if(!dirData.includes(entryName))
-					{
-						dirData.push(entryName);
-					}
-
-					// write meta + data + parent dir meta + parent dir data
-					storage.setItem(fsMetaPrefix+path, JSON.stringify(newMeta));
-					storage.setItem(fsPrefix+path, data);
-					storage.setItem(fsMetaPrefix+dirPath, JSON.stringify(dirMeta));
-					storage.setItem(fsPrefix+dirPath, JSON.stringify(dirData));
-
-					// done
-					resolve();
-				}).catch((error) => {
-					reject("unable to read contents of containing directory: "+error.message);
-				});
-			});
+			// write meta + data + parent dir meta + parent dir data
+			storage.setItem(fsMetaPrefix+path, JSON.stringify(newMeta));
+			storage.setItem(fsPrefix+path, data);
+			storage.setItem(fsMetaPrefix+dirPath, JSON.stringify(dirMeta));
+			storage.setItem(fsPrefix+dirPath, JSON.stringify(dirData));
 		}
 
 		// read the contents of a directory
 		function readDir(path)
 		{
-			return new Promise((resolve, reject) => {
-				readMeta(path).then((meta) => {
-					if(meta == null)
-					{
-						reject(new Error("directory does not exist"));
-						return;
-					}
-					else if(meta.type !== 'dir')
-					{
-						reject(new Error("entry is not a directory"));
-						return;
-					}
+			// read dir meta
+			var meta = readMeta(path);
+			if(meta == null)
+			{
+				throw new Error("directory does not exist");
+			}
+			else if(meta.type !== 'dir')
+			{
+				throw new Error("entry is not a directory");
+			}
 
-					// read directory data
-					var data = storage.getItem(fsPrefix+path);
-					if(data == null)
-					{
-						reject(new Error("missing directory data"));
-						return;
-					}
-					try
-					{
-						data = JSON.parse(data);
-					}
-					catch(error)
-					{
-						reject(new Error("corrupted directory data"));
-						return;
-					}
+			// read directory data
+			var data = storage.getItem(fsPrefix+path);
+			if(data == null)
+			{
+				throw new Error("missing directory data");
+			}
+			try
+			{
+				data = JSON.parse(data);
+			}
+			catch(error)
+			{
+				throw new Error("corrupted directory data");
+			}
 
-					// done
-					resolve(data);
-				}).catch((error) => {
-					reject(error);
-				});
-			});
+			return data;
 		}
 
 		// create a directory
@@ -230,70 +192,53 @@
 		{
 			options = Object.assign({}, options);
 
-			return new Promise((resolve, reject) => {
-				// validate an existing directory
-				readMeta(path).then((meta) => {
-					if(meta != null)
+			var meta = readMeta(path);
+			if(meta != null)
+			{
+				if(meta.type == 'dir')
+				{
+					if(options.ignoreIfExists)
 					{
-						if(meta.type == 'dir')
-						{
-							if(options.ignoreIfExists)
-							{
-								resolve();
-							}
-							else
-							{
-								reject(new Error("directory already exists"));
-							}
-						}
-						else
-						{
-							reject(new Error("entry already exists at path"));
-						}
 						return;
 					}
+					else
+					{
+						throw new Error("directory already exists");
+					}
+				}
+				else
+				{
+					throw new Error("entry already exists at path");
+				}
+			}
 
-					meta = { type: 'dir' };
-
-					writeEntry(path, meta, JSON.stringify([])).then(() => {
-						resolve();
-					}).catch((error) => {
-						reject(error);
-					});
-				}).catch((error) => {
-					reject(error);
-				});
-			});
+			writeEntry(path, {type: 'dir'}, JSON.stringify([]));
 		}
 
 		// read file from a given path
 		function readFile(path)
 		{
-			return new Promise((resolve, reject) => {
-				readMeta(path).then((meta) => {
-					if(meta == null)
-					{
-						reject(new Error("file does not exist"));
-						return;
-					}
-					else if(meta.type !== 'file')
-					{
-						reject(new Error("entry is not a file"));
-						return;
-					}
-					var data = storage.getItem(fsPrefix+path);
-					if(data == null)
-					{
-						reject(new Error("missing file data"));
-						return;
-					}
-					resolve(data);
-				}).catch(reject);
-			});
+			var meta = readMeta(path);
+			if(meta == null)
+			{
+				throw new Error("file does not exist");
+			}
+			else if(meta.type !== 'file')
+			{
+				throw new Error("entry is not a file");
+			}
+
+			var data = storage.getItem(fsPrefix+path);
+			if(data == null)
+			{
+				throw new Error("missing file data");
+				return;
+			}
+			return data;
 		}
 
 		// write file to a given path
-		function writeFile(path, meta, data)
+		function writeFile(path, data)
 		{
 			return writeEntry(path, {type: 'file'}, data);
 		}
