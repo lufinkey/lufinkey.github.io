764d55d6add368f5c5b7d8cf9bc1c649a782e482
lufinkey <luisfinke@gmail.com>
2018-02-28 08:35:54 -0500

    open utility for app defaults

---
+++ system/bin/open.js
@@ -0,0 +1,70 @@
+
+const path = require('path');
+
+if(process.argv.length !== 2)
+{
+	console.error("invalid number of arguments");
+	process.exit(1);
+}
+
+// get filename and extension
+var filename = process.argv[1];
+var extension = path.extname(filename);
+if(extension.startsWith('.'))
+{
+	extension = extension.substring(1, extension.length);
+}
+
+// load app defaults
+var appdefaults = null;
+try
+{
+	appdefaults = JSON.stringify(syscall('filesystem.readFile', '/system/share/appdefaults.js'));
+}
+catch(error)
+{
+	console.error("unable to read app defaults: "+error.message);
+	process.exit(1);
+}
+
+var defaultcmd = appdefaults[extension];
+if(!defaultcmd)
+{
+	console.error("no default app is defined for this file type");
+	process.exit(1);
+}
+else if(!(defaultcmd instanceof Array))
+{
+	console.error("invalid appdefaults entry");
+	process.exit(1);
+}
+else if(defaultcmd.length === 0)
+{
+	console.error("appdefaults entry must have arguments");
+	process.exit(1);
+}
+
+for(var i=1; i<defaultcmd.length; i++)
+{
+	var arg = defaultcmd[i];
+	if(typeof arg !== 'string')
+	{
+		console.error("invalid argument for appdefaults entry");
+		process.exit(1);
+	}
+	else
+	{
+		defaultcmd[i] = arg.replace('%@', filename);
+	}
+}
+
+// launch the process
+syscall('execute', defaultcmd[0], defaultcmd.slice(1)).promise.then(() => {
+	// the process executed successfully
+	process.exit(0);
+}).catch((error) => {
+	// TODO maintain the program's exit code
+	console.error("program execution failed:");
+	console.error(error);
+	process.exit(2);
+});
