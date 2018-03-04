0398b8985f27e3dd15d6017ffb13d07374fafd48
lufinkey <luisfinke@gmail.com>
2018-03-03 19:33:34 -0500

    resizeable windows

--- system/lib/dwm.dll/index.jsx
+++ system/lib/dwm.dll/index.jsx
@@ -4,6 +4,10 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const Window = require('./Window');
 
+
+const minWindowSize = {x: 20, y: 20};
+
+
 class WindowManager extends React.Component
 {
 	constructor(props)
@@ -21,7 +25,8 @@ class WindowManager extends React.Component
 			dragging: null,
 			draggingWindow: null,
 			dragStartPosition: null,
-			draggerStartPosition: null
+			draggerStartPosition: null,
+			draggerStartSize: null
 		};
 
 		// bind methods	
@@ -163,14 +168,48 @@ class WindowManager extends React.Component
 		}
 	}
 
-	minimizeWindow(window)
+	getMouseEventPosition(event)
 	{
-		window.setState({minimized: true});
+		var areaRect = ReactDOM.findDOMNode(this).getBoundingClientRect();
+		return {
+			x: (event.clientX-areaRect.left),
+			y: (event.clientY-areaRect.top)
+		};
 	}
 
-	restoreWindow(window)
+	getMouseEventDragMovement(event)
 	{
-		window.setState({minimized: false});
+		var mousePos = this.getMouseEventPosition(event);
+		return {
+			x: (mousePos.x - this.state.dragStartPosition.x),
+			y: (mousePos.y - this.state.dragStartPosition.y)
+		};
+	}
+
+	startDragging(window, event, what)
+	{
+		var dragStartPosition = this.getMouseEventPosition(event);
+		var draggerStartPosition = Object.assign({}, window.state.position);
+		var draggerStartSize = Object.assign({}, window.state.size);
+
+		this.setState({
+			dragging: what,
+			draggingWindow: window,
+			dragStartPosition: dragStartPosition,
+			draggerStartPosition: draggerStartPosition,
+			draggerStartSize: draggerStartSize
+		});
+	}
+
+	stopDragging()
+	{
+		this.setState({
+			dragging: null,
+			draggingWindow: null,
+			dragStartPosition: null,
+			draggerStartPosition: null,
+			draggerStartSize: null
+		});
 	}
 
 	onWindowTitleBarMouseDown(window, event)
@@ -179,64 +218,149 @@ class WindowManager extends React.Component
 		{
 			if(!this.state.dragging)
 			{
-				var areaRect = ReactDOM.findDOMNode(this).getBoundingClientRect();
-				var dragStartPosition = {
-					x: (event.clientX-areaRect.left),
-					y: (event.clientY-areaRect.top)
-				};
-				this.setState({dragging: 'titlebar', draggingWindow: window, dragStartPosition: dragStartPosition, draggerStartPosition: Object.assign({}, window.state.position)});
+				this.startDragging(window, event, 'titlebar');
+			}
+		}
+	}
+
+	onWindowCornerMouseDown(window, event, corner)
+	{
+		if(event.button == 0)
+		{
+			if(!this.state.dragging)
+			{
+				this.startDragging(window, event, 'corner');
+				this.setState({
+					dragCorner: corner
+				});
 			}
 		}
 	}
 
 	onDocumentMouseMove(event)
 	{
-		switch(this.state.dragging)
+		if(this.state.dragging)
 		{
-			case 'titlebar':
-				var window = this.state.draggingWindow;
-				var position = Object.assign({}, window.state.position);
-				var size = Object.assign({}, window.state.size);
+			let window = this.state.draggingWindow;
+			let position = Object.assign({}, window.state.position);
+			let size = Object.assign({}, window.state.size);
 
-				// find new position
-				var areaRect = ReactDOM.findDOMNode(this).getBoundingClientRect();
-				var movement = {
-					x: (event.clientX - areaRect.left - this.state.dragStartPosition.x),
-					y: (event.clientY - areaRect.top - this.state.dragStartPosition.y)
-				};
-				position.x = this.state.draggerStartPosition.x + movement.x;
-				position.y = this.state.draggerStartPosition.y + movement.y;
+			let movement = this.getMouseEventDragMovement(event);
+			let areaRect = ReactDOM.findDOMNode(this).getBoundingClientRect();
+			
+			switch(this.state.dragging)
+			{
+				case 'titlebar':
+					// set new position
+					position.x = this.state.draggerStartPosition.x + movement.x;
+					position.y = this.state.draggerStartPosition.y + movement.y;
 
-				// ensure we don't go outside the window manager bounds
-				if(position.x < 0)
-				{
-					position.x = 0;
-				}
-				else if((position.x+size.x) > areaRect.width)
-				{
-					position.x = areaRect.width - size.x;
-				}
-				if(position.y < 0)
-				{
-					position.y = 0;
-				}
-				else if((position.y+size.y) > areaRect.height)
-				{
-					position.y = areaRect.height - size.y;
-				}
+					// ensure we don't go outside the window manager bounds
+					if(position.x < 0)
+					{
+						position.x = 0;
+					}
+					else if((position.x+size.x) > areaRect.width)
+					{
+						position.x = areaRect.width - size.x;
+					}
+					if(position.y < 0)
+					{
+						position.y = 0;
+					}
+					else if((position.y+size.y) > areaRect.height)
+					{
+						position.y = areaRect.height - size.y;
+					}
+					break;
+				
+				case 'corner':
+					const resizeX = (mult=1) => {
+						size.x = this.state.draggerStartSize.x + (movement.x*mult);
+						if(size.x < minWindowSize.x)
+						{
+							size.x = minWindowSize.x;
+						}
+					}
+					const resizeY = (mult=1) => {
+						size.y = this.state.draggerStartSize.y + (movement.y*mult);
+						if(size.y < minWindowSize.y)
+						{
+							size.y = minWindowSize.y;
+						}
+					}
+					const reAdjustPositionTop = () => {
+						position.y = this.state.draggerStartPosition.y + this.state.draggerStartSize.y - size.y;
+					}
+					const reAdjustPositionLeft = () => {
+						position.x = this.state.draggerStartPosition.x + this.state.draggerStartSize.x - size.x;
+					}
 
-				window.setState({position: position});
-				break;
+					switch(this.state.dragCorner)
+					{
+						case 'top':
+							resizeY(-1);
+							reAdjustPositionTop();
+							break;
+
+						case 'topleft':
+							resizeX(-1);
+							resizeY(-1);
+							reAdjustPositionLeft();
+							reAdjustPositionTop();
+							break;
+						
+						case 'topright':
+							resizeX(1);
+							resizeY(-1);
+							reAdjustPositionTop();
+							break;
+						
+						case 'left':
+							resizeX(-1);
+							reAdjustPositionLeft();
+							break;
+
+						case 'right':
+							resizeX(1);
+							break;
+
+						case 'bottomleft':
+							resizeX(-1);
+							resizeY(1);
+							reAdjustPositionLeft();
+							break;
+
+						case 'bottomright':
+							resizeX(1);
+							resizeY(1);
+							break;
+
+						case 'bottom':
+							resizeY(1);
+							break;
+					}
+					break;
+			}
+
+			// update window state
+			window.setState({position: position, size: size});
 		}
 	}
 
 	onDocumentMouseUp(event)
 	{
-		switch(this.state.dragging)
+		if(this.state.dragging)
 		{
-			case 'titlebar':
-				this.setState({dragging: null, draggingWindow: null, dragStartPosition: null, draggerStartPosition: null});
-				break;
+			switch(this.state.dragging)
+			{
+				case 'corner':
+					this.setState({
+						dragCorner: null
+					});
+					break;
+			}
+			this.stopDragging();
 		}
 	}
 
@@ -259,7 +383,8 @@ class WindowManager extends React.Component
 				onMount={(window) => {this.onWindowMount(window)}}
 				onUnmount={(window) => {this.onWindowUnmount(window)}}
 				onWillUpdate={() => {this.onWindowWillUpdate(this.windows[windowId])}}
-				onTitleBarMouseDown={(event) => {this.onWindowTitleBarMouseDown(this.windows[windowId], event)}}/>
+				onTitleBarMouseDown={(event) => {this.onWindowTitleBarMouseDown(this.windows[windowId], event)}}
+				onCornerMouseDown={(event, corner) => {this.onWindowCornerMouseDown(this.windows[windowId], event, corner)}}/>
 		);
 	}
 }
