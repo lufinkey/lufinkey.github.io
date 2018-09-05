e86f81dffc77bc70cfcf6f1e9d395504c1236fbe
lufinkey <luisfinke@gmail.com>
2018-09-05 00:10:57 -0400

    re-add startup audio

--- system/boot/bootScreen.jsx
+++ system/boot/bootScreen.jsx
@@ -3,6 +3,8 @@ const SystemUI = require('sysui');
 const React = require('react');
 const DefaultBootScreen = require('./screens/DefaultBootScreen');
 
+const startupAudio = new Audio('system/boot/audio/startup.mp3');
+
 
 class BootScreen extends React.Component
 {
@@ -18,5 +20,6 @@ SystemUI.register('boot', BootScreen);
 
 process.stdin.on('end', () => {
 	SystemUI.unregister('boot');
+	startupAudio.play();
 	process.exit(0);
 });
