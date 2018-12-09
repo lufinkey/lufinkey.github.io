4ef2a6b6481eb01e18aa6352ae1f00c11954d0bd
lufinkey <luisfinke@gmail.com>
2018-12-08 19:12:06 -0500

    intercept link clicks and open in new tab

--- system/bin/shell32.exe/index.jsx
+++ system/bin/shell32.exe/index.jsx
@@ -1,12 +1,26 @@
 
 const React = require('react');
+const ReactDOM = require('react-dom');
 const Display = require('displaymgrclient');
 const SelfAd = require('selfad');
 const Desktop = require('./Desktop');
+const { spawn } = require('child_process');
 requireCSS('./style.css');
 
 class Shell32 extends React.Component
 {
+	componentDidMount() {
+		const element = ReactDOM.findDOMNode(this);
+		// add click handler for links
+		element.addEventListener('click', (event) => {
+			const clickedElement = event.target;
+			if(clickedElement.nodeName === 'A' && clickedElement.target === '_blank' && typeof clickedElement.href === 'string') {
+				event.preventDefault();
+				spawn('iframe', [ clickedElement.href ], {detached: true});
+			}
+		}, {capture: true});
+	}
+
 	render() {
 		return (
 			<div className="shell32">
